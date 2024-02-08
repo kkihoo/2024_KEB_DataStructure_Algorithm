129757071180db7d7f656bd31c6d624363026967
Kihun Kim <kimkihun811@gmail.com>
2024-02-08 11:27:52 +0900

    mymath v0.3 timer inner

--- Day 01/mymath.py
+++ Day 01/mymath.py
@@ -1,5 +1,14 @@
 import time
 
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"elapsed time : {end - start}")
+        return result
+    return wrapper
+
 def factorial (number) -> int :
     """_summary_
     factorial by repetition
@@ -28,7 +37,8 @@ def factorial (number) -> int :
 #     else :
 #         return number * factorial(number-1)
 
-def nCr(n, r) -> int :
+@timer
+def nCr(n, r) -> int : # SRP, OCP violation
     """_summary_
     조합함수
 
@@ -39,9 +49,9 @@ def nCr(n, r) -> int :
     Returns:
         int: 순서 상관없이 선택하는 경우의 수
     """
-    start = time.time()
+
     numerator = factorial(n)
     denominator = factorial(n-r) * factorial(r)
-    end = time.time()
-    print(f"소요시간 : {end - start}")
+
     return int(numerator / denominator)
+
