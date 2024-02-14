7e4e744c00f96dc3f7d324c04628933b43e2fdd6
Kihun Kim <kimkihun811@gmail.com>
2024-02-14 16:14:21 +0900

    decimal_2_octal v2

--- Day 03/dec_to_oct.py
+++ Day 03/dec_to_oct.py
@@ -8,11 +8,10 @@ def decimal_to_octal(number = int) -> str:
     Returns:
         str: base octal
     """
-    octal = " "
-    while number > 0:
-        octal = str(number % 8) + octal
-        number = number // 8
-    return octal
+    if number < 8:
+        return str(number)
+    else:
+        return decimal_to_octal(n // 8) + str(n % 8)
 
 n = int(input("Input Decimal Number : "))
 print(decimal_to_octal(n))
